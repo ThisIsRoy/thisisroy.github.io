9dbf9f4bdf1350cda6162b9cd343fd1ae42b366a
Roy <rs58@rice.edu>
2018-03-13 14:43:01 -0500

    created sticky nav

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-This is the repo for my portfolio website that I coded from scratch with the help of Bootstrap 4.
+This is the repo for my portfolio website that I coded from scratch.
 
 Once it's done, it will be hosted on surge! 
--- js/app.js
+++ js/app.js
@@ -6,37 +6,51 @@ $(window).on('load', function () {
   $('#moreButton').addClass('buttonFadein');
 });
 
+$(document).ready(() =>{
+  // Animates scrolling motion 
+  $('#see-more').click(function () {
+    $('html, body').animate({
+      scrollTop: $('.project-section').offset().top
+    }, 2000);
+  });
 
-// Animates scrolling motion 
-$('#see-more').click(function() {
-  $('html, body').animate({
-    scrollTop: $('.project-section').offset().top
-  }, 2000);
+  $('#home-section').click(function () {
+    $('html, body').animate({
+      scrollTop: $('header').offset().top
+    }, 2000);
+  });
+
+  $('#about-link').click(function () {
+    $('html, body').animate({
+      scrollTop: $('.about-section').offset().top - 50
+    }, 1500);
+  });
+
+  $('#portfolio-link').click(function () {
+    $('html, body').animate({
+      scrollTop: $('.project-section').offset().top
+    }, 2000);
+  });
+
+  $('#skills-link').click(function () {
+    $('html, body').animate({
+      scrollTop: $('.logo-section').offset().top - 50
+    }, 2000);
+  });
+
+  // Sticky navbar 
+  $(".about-section").waypoint(function (direction) {
+    if (direction === "down") {
+      $(".my-nav").addClass("sticky-nav");
+    } else {
+      $(".my-nav").removeClass("sticky-nav");
+    }
+  }, {
+      offset: '60px;'
+  });
 });
 
-$('#home-section').click(function () {
-  $('html, body').animate({
-    scrollTop: $('header').offset().top
-  }, 2000);
-});
 
-$('#about-link').click(function () {
-  $('html, body').animate({
-    scrollTop: $('.about-section').offset().top
-  }, 1500);
-});
-
-$('#portfolio-link').click(function () {
-  $('html, body').animate({
-    scrollTop: $('.project-section').offset().top
-  }, 2000);
-});
-
-$('#skills-link').click(function () {
-  $('html, body').animate({
-    scrollTop: $('.logo-section').offset().top
-  }, 2000);
-});
 
 
 
